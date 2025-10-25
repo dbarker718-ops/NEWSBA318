a28afa742c4a05b33ee25ea0dfc492d06c30bafb
dbarker718-ops <dbarker718@gmail.com>
2025-10-25 15:40:43 -0400

    created statuses

--- routes/customers.js
+++ routes/customers.js
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { json } from "express";
 import customers from "../data/customers.js";
 const router = express.Router();
 
-router.route("/")
+router
+  .route("/")
   .get((req, res) => {
     res.json(customers);
   })
@@ -10,19 +11,16 @@ router.route("/")
     console.log(req.body);
     if (req.body.username && req.body.email) {
       if (customers.find((u) => u.username == req.body.username)) {
-        res.json({ error: "Username Already Taken" });
+        res.status(409).json({ error: "Username Already Taken" });
         return;
       }
-      console.log("Hi");
       const customer = {
         id: customers[customers.length - 1].id + 1,
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
       };
-      console.log("hello");
       customers.push(customer);
-      console.log("added customer")
       res.json(customers[customers.length - 1]);
     } else res.json({ error: "Insufficient Data" });
   });
