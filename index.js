dc251f9ecc25fe786ac0c592c6fe11ccbd494ff4
dbarker718-ops <dbarker718@gmail.com>
2025-10-25 14:07:40 -0400

    error handler added

--- index.js
+++ index.js
@@ -1,195 +1,85 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-const stripe = require ('stripe')('prod_TCqoJcOV8TWUzH');
-
 
 app.use(
   express.json({
-    verify: (req, res, buffer) => (req['rawBody'] = buffer),
+    verify: (req, res, buffer) => (req["rawBody"] = buffer),
   })
 );
 
-
-const customers = {
-  
-  'stripeCustomerId': {
-    apiKey: '123xyz',
-    active: false,
-    itemId: 'stripeSubscriptionItemId',
+const customers = [
+  {
+    id: 1,
+    username: "Jack",
+    email: "Jack@gmail.com",
+    password: "jack123",
   },
-};
-const apiKeys = {
- 
-  '123xyz': 'stripeCustomerId',
-};
+  {
+    id: 2,
+    username: "Bill",
+    email: "Billy@gmail.com",
+    password: "billy123",
+  },
+  {
+    id: 3,
+    username: "Jill",
+    email: "Jill@gmail.com",
+    password: "jill123",
+  },
+];
 
+const payments = [
+  {
+    id: 1,
+    user_id: 1,
+    company: "Discover",
+    type: "credit",
+  },
+  {
+    id: 2,
+    user_id: 2,
+    company: "TD Bank",
+    type: "debit",
+  },
+  {
+    id: 3,
+    user_id: 3,
+    company: "Master Card",
+    type: "credit",
+  },
+];
 
-function generateAPIKey() {
-  const { randomBytes } = require('crypto');
-  const apiKey = randomBytes(16).toString('hex');
-  const hashedAPIKey = hashAPIKey(apiKey);
+const listing = [
+  {
+    id: 1,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean malesuada massa ac odio euismod, vel convallis mauris placerat. Vestibulum ante.",
+    bedrooms: 1,
+    bathrooms: 1,
+  },
+  {
+    id: 1,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean malesuada massa ac odio euismod, vel convallis mauris placerat. Vestibulum ante.",
+    bedrooms: 2,
+    bathrooms: 1.5,
+  },
+  {
+    id: 1,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean malesuada massa ac odio euismod, vel convallis mauris placerat. Vestibulum ante.",
+    bedrooms: 3,
+    bathrooms: 2,
+  },
+];
 
- 
-  if (apiKeys[hashedAPIKey]) {
-    generateAPIKey();
-  } else {
-    return { hashedAPIKey, apiKey };
-  }
-}
-
-
-function hashAPIKey(apiKey) {
-  const { createHash } = require('crypto');
-
-  const hashedAPIKey = createHash('sha256').update(apiKey).digest('hex');
-
-  return hashedAPIKey;
-}
-
-
-app.post('/checkout', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    mode: 'subscription',
-    payment_method_types: ['card'],
-    line_items: [
-      {
-        price: 'price_YOUR-PRODUCT',
-      },
-    ],
-   
-    success_url:
-      'test',
-    cancel_url: 'test',
-  });
-
-  res.send(session);
+app.get("/", (req, res) => {
+  res.send("Payment API");
 });
 
-
-app.post('/webhook', async (req, res) => {
-  let data;
-  let eventType;
-  
-  const webhookSecret = 'whsec_YOUR-KEY';
-
-  if (webhookSecret) {
-    
-    let event;
-    let signature = req.headers['stripe-signature'];
-
-    try {
-      event = stripe.webhooks.constructEvent(
-        req['rawBody'],
-        signature,
-        webhookSecret
-      );
-    } catch (err) {
-      console.log(`  Webhook signature verification failed.`);
-      return res.sendStatus(400);
-    }
-   
-    data = event.data;
-    eventType = event.type;
-  } else {
-    
-    data = req.body.data;
-    eventType = req.body.type;
-  }
-
-  switch (eventType) {
-    case 'checkout.session.completed':
-      console.log(data);
-     
-      const customerId = data.object.customer;
-      const subscriptionId = data.object.subscription;
-
-      console.log(
-        `💰 Customer ${customerId} subscribed to plan ${subscriptionId}`
-      );
-
-     
-      const subscription = await stripe.subscriptions.retrieve(subscriptionId);
-      const itemId = subscription.items.data[0].id;
-
-      
-      const { apiKey, hashedAPIKey } = generateAPIKey();
-      console.log(`User's API Key: ${apiKey}`);
-      console.log(`Hashed API Key: ${hashedAPIKey}`);
-
-      
-      customers[customerId] = {
-        apikey: hashedAPIKey,
-        itemId,
-        active: true,
-      };
-      apiKeys[hashedAPIKey] = customerId;
-
-      break;
-    case 'invoice.paid':
-     
-      break;
-    case 'invoice.payment_failed':
-      
-      break;
-    default:
-   
-  }
-
-  res.sendStatus(200);
+// Error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ error: err.message });
 });
 
-
-app.get('/customers/:id', (req, res) => {
-  const customerId = req.params.id;
-  const account = customers[customerId];
-  if (account) {
-    res.send(account);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-
-app.get('/api', async (req, res) => {
-  const { apiKey } = req.query;
- 
-
-  if (!apiKey) {
-    res.sendStatus(400); 
-  }
-
-  const hashedAPIKey = hashAPIKey(apiKey);
-
-  const customerId = apiKeys[hashedAPIKey];
-  const customer = customers[customerId];
-
-  if (!customer || !customer.active) {
-    res.sendStatus(403); 
-  } else {
-
-  
-    const record = await stripe.subscriptionItems.createUsageRecord(
-      customer.itemId,
-      {
-        quantity: 1,
-        timestamp: 'now',
-        action: 'increment',
-      }
-    );
-    res.send({ data: 'Successful', usage: record });
-  }
-});
-
-app.get('/usage/:customer', async (req, res) => {
-  const customerId = req.params.customer;
-  const invoice = await stripe.invoices.retrieveUpcoming({
-    customer: customerId,
-  });
-
-  res.send(invoice);
-});
-
-app.listen(3000, () => console.log('alive on http://localhost:3000/api'));
-
-
+app.listen(3000, () => console.log("alive on http://localhost:3000/api"));
