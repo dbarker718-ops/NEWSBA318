83689c7964e16da489036e42438a50cbaa9565f7
dbarker718-ops <dbarker718@gmail.com>
2025-10-27 15:58:45 -0400

    added view, patch

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ import listingRouter from "./routes/listings.js";
 
 const app = express();
 
+app.set('view engine', 'ejs'); 
+
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-  res.send("Payment API");
+  res.render("index");
 });
 
 app.use("/api/customers", customerRouter);
--- routes/listings.js
+++ routes/listings.js
@@ -10,7 +10,6 @@ router
   .post((req, res) => {
     console.log(req.body);
     if (req.body.description && req.body.bedrooms && req.body.bathrooms) {
-      
       const listing = {
         id: listings[listings.length - 1].id + 1,
         description: req.body.description,
@@ -20,5 +19,25 @@ router
       listings.push(listing);
       res.status(201).json(listings[listings.length - 1]);
     } else res.status(400).json({ error: "Insufficient Data" });
+  });
+
+router
+  .route("/:id")
+  .patch((req, res, next) => {
+    const listing = listings.find((l) => l.id == req.params.id);
+    if (listing) res.json(listing);
+    else next({ status: 404, error: "Listing not found" });
+  })
+  .patch((req, res, next) => {
+    const listing = listings.find((l, i) => {
+      if (l.id == req.params.id) {
+        for (const key in req.body) {
+          posts[i][key] = req.body[key];
+        }
+        return true;
+      }
+    });
+    if (listing) res.json(listing);
+    else next({ status: 404, error: "Listing not found" });
   });
 export default router;
